37b588eae8aac45a92ca0a1839a6973c4137701c
NShevchik <n.shevchik@softteco.com>
2022-11-29 13:51:24 +0300

    test-send-data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+// const sendDataToReactNativeApp = async () => {
+//   window.postMessage("It is Data from WebView / Website");
+// };
 const sendDataToReactNativeApp = async () => {
-  window.postMessage("It is Data from WebView / Website");
+  window.ReactNativeWebView.postMessage("Data from WebView / Website");
 };
 
 window.addEventListener("message", (message) => {
   alert(message.data);
 });
-alert("hello");
+
 function App() {
   return (
     <div className="App">
