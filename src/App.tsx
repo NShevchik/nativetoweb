9e60381a2466d64f0e82d8cff39e1c72d75cd0ce
NShevchik <n.shevchik@softteco.com>
2022-11-29 14:07:50 +0300

    donetest

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-const sendDataToReactNativeApp = async () => {
-  window.postMessage("It is Data from WebView / Website");
-};
-
-// window.addEventListener("message", (message) => {
-//   alert(message.data);
-// });
-
 function App() {
+  const [message, setMessage] = useState("");
+
+  const sendDataToReactNativeApp = async () => {
+    window.postMessage("It is Data from WebView / Website");
+  };
+
+  window.addEventListener("message", (message) => {
+    alert(message.data);
+    setMessage(message.data);
+  });
+
   return (
     <div className="App">
       <p>Webview to native</p>
+      <p>{message}</p>
       <button onClick={() => sendDataToReactNativeApp()}>
         Send Data To React Native App
       </button>
